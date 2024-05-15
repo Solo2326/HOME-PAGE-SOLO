4bf1fc39c973f9d626f788fb7655a020c86c4b30
Solo2326 <162106343+Solo2326@users.noreply.github.com>
2024-05-15 10:44:15 +0200

    Update chisono.js

--- chisono.js
+++ chisono.js
@@ -1,4 +1,4 @@
-gsap.from("#chi-sono h2, #chi-sono p", {
+gsap.from("chisono.html h2, chisono.html p", {
     opacity: 0,
     y: 50,
     duration: 1,
@@ -7,5 +7,32 @@ gsap.from("#chi-sono h2, #chi-sono p", {
         start: "top center",
         end: "bottom center",
         scrub: true, 
+    }
+});
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray("chisono.html h2, chisono.html p, chisono.html .skill-list").forEach((elem) => {
+    gsap.from(elem, {
+        opacity: 0,
+        y: 50,
+        duration: 1,
+        ease: "power2.out",
+        scrollTrigger: {
+            trigger: elem,
+            start: "top center+=100",
+            toggleActions: "play none none reverse"
+        }
+    });
+});
+
+gsap.from("chisono.html .parallax-image", {
+    opacity: 0,
+    x: -50,
+    duration: 1.5,
+    ease: "power2.out",
+    scrollTrigger: {
+        trigger: "chisono.html .parallax-image",
+        start: "top center+=100",
+        toggleActions: "play none none reverse"
     }
 });
